1a4584aa31ef21018c97b001654a465fd4b05c61
Hernan Guzman <hernandgr@gmail.com>
2018-01-29 19:59:05 -0500

    Added initial code

---
+++ src/TestData.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TestData.Api.Models;
+
+namespace TestData.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly List<Employee> employees = new List<Employee>
+        {
+            new Employee { Id= 1, Name= "Juan", RoleId = 1, RoleName = "Administrator", AnnualSalary= 60000m},
+            new Employee { Id= 2, Name= "Sebastian", RoleId = 2, RoleName = "Contractor", AnnualSalary= 80000m}
+        };
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var employee = employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ src/TestData.Api/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace TestData.Api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public decimal AnnualSalary { get; set; }
+    }
+}
